4841b79e131c2012fe9ba1babdff77a9fd08f989
Liigo Zhuang <liigo@qq.com>
2024-07-04 19:53:52 +0800

    07_threads, Clone

--- exercises/07_threads/01_threads/src/lib.rs
+++ exercises/07_threads/01_threads/src/lib.rs
@@ -15,7 +15,32 @@
 use std::thread;
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+    if v.len() == 0 {
+        return 0; // 我一开始忽视了len==0的情况，导致后面split_at()时panic
+    }
+    if v.len() == 1 {
+        return v[0];
+    }
+    let (v1, v2) = v.split_at(v.len() / 2);
+    // 20240704
+    // 此时v1 v2都是引用了v，无法在spawn内使用，需要先clone
+    // let v1 = v1.clone(); 不行，只是复制引用，v1还是v1。!!这里有点反直觉吧??
+    // let v1 = &*v1; // 不行，v1还是v1
+    // let v1 = &(*v1).clone(); 不行，[i32]没有clone方法，编译不过
+    // let v1 = &(*v1.clone()); 不行，还是复制引用，没有意义
+    // 总结：spawn不接受引用，必须给他一个值；[i32]是!Sized是无法传递的值，也不被接受。
+    // 总结：看spawn参数定义，F: FnOnce() -> T + Send + 'static。引用不满足'static, [i32]不满足Send。
+    // 那怎么克隆&[i32]呢，可以转换成Vec或Box。
+    // 前面提到反直觉的问题，为什么&i32可以克隆而&[i32]不能被克隆呢？
+    //   我感觉Rust里面的逻辑是，如果T:Clone，t:&T，则t.clone()返回T，否则t.clone()返回&T。
+    //   即编译器使用`impl Clone for T`优先级高于`impl<T: ?Sized> Clone for &T`。
+    //   rustc输出可以反证我上述推论："note: `A` does not implement `Clone`, so `&A` was cloned instead"。
+    //   在本案中，[i32]不满足:Sized，因而不可能实现Clone，故&[i32]只能复制引用。这样解释就通了。
+    let v1 = v1.to_vec();
+    let v2 = v2.to_vec();
+    let t1 = thread::spawn(move || v1.iter().sum::<i32>());
+    let t2 = thread::spawn(move || v2.iter().sum::<i32>());
+    t1.join().unwrap() + t2.join().unwrap()
 }
 
 #[cfg(test)]
