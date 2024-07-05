d7b9854f813d3d1e0f33a93526f9ef269a12a0b6
Liigo Zhuang <liigo@qq.com>
2024-07-05 18:34:38 +0800

    07_threads, channel()

--- exercises/07_threads/08_client/src/lib.rs
+++ exercises/07_threads/08_client/src/lib.rs
@@ -7,23 +7,37 @@ pub mod store;
 
 #[derive(Clone)]
 // TODO: flesh out the client implementation.
-pub struct TicketStoreClient {}
+pub struct TicketStoreClient {
+    sender: Sender<Command>
+}
 
 impl TicketStoreClient {
     // Feel free to panic on all errors, for simplicity.
     pub fn insert(&self, draft: TicketDraft) -> TicketId {
-        todo!()
+        // 20240705: 创建临时channel用于接收服务端的执行结果，这样是最佳实践吗？
+        // 看来channel是单向通讯，本例中客户端用channel1向服务端发送请求，服务端回复数据要用另一个channel2。
+        // channel传输的数据是静态强类型的，因而也不好做双向通讯吧。
+        // 只要channel的创建和传输是轻量级的(官方文档未提及)，引入临时channel也无所谓。
+        // 本例中channel定义时就决定了传输类型是T:TicketId，服务端必然发送T，客户端必然接收T，编译器予以保证。
+        // 明确写成 channel::<TicketId>() 更好，有了类型推断可以省掉::<TicketId>。
+        let (s, r) = std::sync::mpsc::channel();
+        self.sender.send(Command::Insert { draft, response_channel: s }).unwrap();
+        r.recv().unwrap()
     }
 
     pub fn get(&self, id: TicketId) -> Option<Ticket> {
-        todo!()
+        let (s, r) = std::sync::mpsc::channel();
+        self.sender.send(Command::Get { id, response_channel: s }).unwrap();
+        r.recv().unwrap()
     }
 }
 
 pub fn launch() -> TicketStoreClient {
     let (sender, receiver) = std::sync::mpsc::channel();
     std::thread::spawn(move || server(receiver));
-    todo!()
+    TicketStoreClient {
+        sender,
+    }
 }
 
 // No longer public! This becomes an internal detail of the library now.
